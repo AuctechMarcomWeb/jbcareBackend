71900836d795c3f8e8692cb5b036bef97a320c05
ShubhangiAuctech <shubhangi.sri@auctech.in>
2025-11-12 16:11:48 +0530

    Landlord changes

--- models/LandLord.modal.js
+++ models/LandLord.modal.js
@@ -3,10 +3,27 @@ import mongoose from "mongoose";
 const landlordSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
+    fatherOrSpouseName: { type: String, trim: true },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: true,
+    },
+    dob: { type: Date },
     phone: { type: String, required: true },
+    alternateMobileNumber: { type: String },
     email: { type: String, trim: true, lowercase: true },
     address: { type: String },
+    coorespondenceAddress: { type: String },
     profilePic: { type: String },
+    idProof: {
+      type: {
+        type: String,
+        enum: ["Aadhaar", "PAN", "Passport", "Driving License", "Other"],
+      },
+      number: { type: String, trim: true },
+      documentUrl: { type: String }, // optional image/pdf link
+    },
     siteId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Site",
@@ -23,12 +40,31 @@ const landlordSchema = new mongoose.Schema(
         ref: "Unit",
       },
     ],
-
+    propertyDetails: {
+      propertyName: { type: String },
+      propertyType: {
+        type: String,
+        enum: ["Apartment", "House", "Shop", "Office", "Other"],
+      },
+      propertyAddress: { type: String },
+      propertyDocumentsUrl: [{ type: String }], // array for multiple uploads
+    }, // additional property details
     // ownership status
     isActive: { type: Boolean, default: true }, // false = archived
     ownershipStartDate: { type: Date, default: Date.now },
     ownershipEndDate: { type: Date },
+    bankDetails: {
+      accountHolderName: { type: String },
+      bankName: { type: String },
+      accountNumber: { type: String },
+      ifscCode: { type: String },
+      branchAddress: { type: String },
+    },
 
+    // 📞 Emergency & Misc Details
+    emergencyContactName: { type: String },
+    emergencyContactNumber: { type: String },
+    notes: { type: String },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User", // whoever registered this landlord
