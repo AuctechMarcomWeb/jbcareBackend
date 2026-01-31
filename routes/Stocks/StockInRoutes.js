91e8893eb296cc984aa8778d750d141c89a3cc4a
ShubhangiSrivastava09 <shubhangi.sri09@gmail.com>
2026-01-31 15:21:03 +0530

    gitsummary

--- routes/Stocks/StockInRoutes.js
+++ routes/Stocks/StockInRoutes.js
@@ -4,6 +4,7 @@ import {
   deleteStockIn,
   getStockInCountStats,
   getStockInList,
+  getStockSummary,
   performStockOut,
   updateStockIn,
 } from "../../controllers/stockInController.js";
@@ -13,6 +14,7 @@ const router = express.Router();
 router.post("/", createStockIn);
 router.post("/stockout", performStockOut);
 router.get("/", getStockInList);
+router.get("/getStockSummary", getStockSummary);
 router.get("/getStockInCountStats", getStockInCountStats);
 router.put("/:id", updateStockIn);
 router.delete("/:id", deleteStockIn);
